51d9e42027f941ec6edffba2e30fae0a545b46e6
Derick Sayavong <dsayavong54651@gmail.com>
2023-09-25 23:38:50 -0600

    setup

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import customtkinter as ctk
-
+from settings import *
 try:
-    from ctypes import windll
+    from ctypes import windll, byref, sizeof, c_int
 
 except:
     pass
@@ -9,18 +9,32 @@ except:
 
 class App(ctk.CTk):
     def __init__(self):
-        super().__init__()
 
-        self.geometry('1000x600')
+        self.color = WEATHER_DATA['Clear']
+
+        super().__init__(fg_color = self.color['main'])
+        self.change_title_bar(self.color['title'])
+        
+        self.geometry('550x250')
+        self.minsize(550,250)
         self.iconbitmap('empty.ico')
         self.title('')
         
+
 
 
         #* RUN
         self.mainloop()
     
 
-    def change_title_bar
+    def change_title_bar(self, color):
+        try:
 
-App()
+            HWND = windll.user32.GetParent(self.winfo_id())
+            windll.dwmapi.DwmSetWindowAttribute(HWND, 35, byref(c_int(color)), sizeof(c_int))
+
+        except:
+            pass
+
+if __name__ == '__main__':
+    App()

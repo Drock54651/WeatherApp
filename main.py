00125d72ca7ae8edf9421050d050a665033bd715
Derick Sayavong <dsayavong54651@gmail.com>
2023-09-27 23:10:32 -0600

    comments

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ class App(ctk.CTk):
         #* STATES
         self.height_break = 600
         self.width_break = 1000
+    
         self.full_height_bool = ctk.BooleanVar(value = False) #! checks if window size exceeds the height break of 600px
         self.full_width_bool = ctk.BooleanVar(value = False)  #! Check width break
         self.bind('<Configure>', self.check_size)
@@ -55,11 +56,11 @@ class App(ctk.CTk):
         if event.widget == self: #! prevents calling configure on every widget, this ensures only the main window is applied
             
             #* WIDTH
-            if self.full_width_bool.get(): #! if window width > 600 px
+            if self.full_width_bool.get(): #! if window width > 600 px, check if the width becomes smaller or equal to 600
                 if event.width < self.width_break:
                     self.full_width_bool.set(False)
             
-            else: #! window width < 600 px 
+            else: #! window width < 600 px, check if the width becomes bigger or equal to 600
                 if event.width > self.width_break:
                     self.full_width_bool.set(True)
 
@@ -74,7 +75,7 @@ class App(ctk.CTk):
 
     def change_size(self, *args):
         self.widget.pack_forget()
-        
+
         #* MAX WIDGET
         if self.full_height_bool.get() and self.full_width_bool.get():
             self.widget = MaxWidget(self)
